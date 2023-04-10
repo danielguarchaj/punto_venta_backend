3164f8e6882c7e06888598f90e2f7e7d378f166c
Daniel Guarchaj <gtdanniel@gmail.com>
2023-04-10 00:09:27 -0600

    Add null to models

--- inventory/models.py
+++ inventory/models.py
@@ -4,21 +4,21 @@ from django.contrib.auth.models import User
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     sale_price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
